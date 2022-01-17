779e2c99eae434a5dbe87ed1a30aea192b180611
Jon <jonathan.p.lamar@gmail.com>
2022-01-16 17:26:59 -0700

    Leaving myself a note

--- lua/config/nvim-autopairs.lua
+++ lua/config/nvim-autopairs.lua
@@ -3,4 +3,5 @@ require('nvim-autopairs').setup {
   disable_in_macro = true,
   -- disable when insert after visual block mode
   disable_in_visualblock = true,
+  -- TODO: Ignore certain unpaired delimiters, eg in comments or strings.
 }
