63be532d8c55f4b78f67838a0be4fa303aca5f9f
Jonathan Lamar <jonalarm@amazon.com>
2022-01-31 16:04:53 +0000

    Better barbar

--- lua/config/barbar.lua
+++ lua/config/barbar.lua
@@ -48,7 +48,7 @@ vim.g.bufferline = {
   maximum_padding = 1,
 
   -- Sets the maximum buffer name length.
-  maximum_length = 30,
+  maximum_length = 100,
 
   -- If set, the letters for each buffer in buffer-pick mode will be
   -- assigned based on their name. Otherwise or in case all letters are
