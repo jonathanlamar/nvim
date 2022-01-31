dcfcbdc4ff10615b82492b96e3a2852fca8bba63
Jon <jonathan.p.lamar@gmail.com>
2022-01-31 07:52:31 -0700

    Went with slightly modified defaults instead.

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -9,9 +9,23 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {
+      {
+        'branch',
+        icon = ''
+      },
+      {
+        'diff',
+        symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+      },
+      {
+        'diagnostics',
+        sources = { 'coc' },
+        symbols = { error = ' ', warn = ' ', info = ' ' },
+      },
+    },
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'encoding', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
