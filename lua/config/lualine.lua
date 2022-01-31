b097ba301d85f793806955ebce2a5461d682b81e
Jon <jonathan.p.lamar@gmail.com>
2022-01-31 08:09:00 -0700

    Added conditions to hide sections for narrow vsplits.

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,3 +1,8 @@
+-- Condition to hide a section for narrow windows.
+local not_too_wide = function()
+  return vim.fn.winwidth(0) > 80
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -12,27 +17,33 @@ require('lualine').setup {
     lualine_b = {
       {
         'branch',
-        icon = ''
+        icon = '',
+        cond = not_too_wide,
       },
       {
         'diff',
         symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+        cond = not_too_wide,
       },
       {
         'diagnostics',
         sources = { 'coc' },
         symbols = { error = ' ', warn = ' ', info = ' ' },
+        cond = not_too_wide,
       },
     },
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'filetype'},
+    lualine_c = {{ 'filename', cond = not_too_wide }},
+    lualine_x = {
+      { 'encoding', cond = not_too_wide },
+      { 'filetype', cond = not_too_wide },
+    },
     lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_z = {'location'},
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{ 'filename', cond = not_too_wide }},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
