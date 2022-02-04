9c004e31871ea79f4bfa1d089e9824f12e665f86
Jonathan Lamar <jonathan.p.lamar@gmail.com>
2022-02-04 16:49:20 -0700

    Messing around with onedark

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -17,10 +17,10 @@ require("onedark").setup({
 	style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 	transparent = false, -- Show/hide background
 	term_colors = true, -- Change terminal color as per the selected theme style
-	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+	ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
 	-- toggle theme style ---
-	toggle_style_key = "<leader>to", -- Default keybinding to toggle
-	toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+	-- toggle_style_key = "<leader>to", -- Default keybinding to toggle
+	-- toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
 
 	-- Change code style ---
 	-- Options are italic, bold, underline, none
