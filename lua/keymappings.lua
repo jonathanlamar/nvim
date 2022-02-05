e8f10355df00313f06566741f8de23598f38051d
Jonathan Lamar <jonalarm@amazon.com>
2022-02-05 15:24:21 +0000

    Works with mac keys

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -36,9 +36,9 @@ keymap("n", "<C-Right>", "<Nop>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-M-l>", ":BufflerLineMoveNext<cr>", opts)
+keymap("n", "<S-Right>", ":BufferLineMoveNext<cr>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
-keymap("n", "<S-M-h>", ":BufflerLineMovePrev<cr>", opts)
+keymap("n", "<S-Left>", ":BufferLineMovePrev<cr>", opts)
 
 -- Insert mode nav keys
 keymap("i", "<C-k>", "<Up>", opts)
