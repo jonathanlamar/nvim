2b98b5669f02da761d948503a289b1e621e5226e
Jonathan Lamar <jonalarm@amazon.com>
2022-02-17 14:24:06 +0000

    toggleterm settings

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -7,7 +7,7 @@ local window_size = function(term)
 	if term.direction == "horizontal" then
 		return 15
 	elseif term.direction == "vertical" then
-		return math.floor(vim.o.columns * 0.4)
+		return math.floor(vim.o.columns * 0.5)
 	end
 end
 
