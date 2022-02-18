1758c2da2ea5a88a504a69b5416c06725accd9a0
Jon <jonathan.p.lamar@gmail.com>
2022-02-18 12:13:11 -0700

    Using style variable

--- lua/colorschemes/onedark.lua
+++ lua/colorschemes/onedark.lua
@@ -5,7 +5,7 @@ local lsp_ref_bg = palette[style].bg2
 
 require("onedark").setup({
 	-- Main options --
-	style = "warmer", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	style = style, -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 	transparent = false, -- Show/hide background
 	term_colors = true, -- Change terminal color as per the selected theme style
 	ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
