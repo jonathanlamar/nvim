5ae191d89993722af8bf18fd9ccb319e0f227163
Jonathan Lamar <jonalarm@amazon.com>
2022-02-24 21:09:25 +0000

    No more powerline style seps

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -22,8 +22,8 @@ lualine.setup({
     options = {
         icons_enabled = true,
         theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = { "alpha", "dashboard", "coc-explorer", "Outline" },
         always_divide_middle = true,
     },
