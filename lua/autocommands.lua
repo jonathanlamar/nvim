bb5bef94cb1205fdd03fa6af822bad527d29000f
Jon <jonathan.p.lamar@gmail.com>
2022-12-25 12:38:32 -0600

    Formatting

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -7,13 +7,14 @@ function _G.set_terminal_keymaps()
     vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 
+local utils = require("nvim-tree.utils")
 vim.api.nvim_create_autocmd("BufEnter", {
     nested = true,
     callback = function()
-        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-            vim.cmd "quit"
+        if #vim.api.nvim_list_wins() == 1 and utils.is_nvim_tree_buf() then
+            vim.cmd("quit")
         end
-    end
+    end,
 })
 
 -- Don't autocomment new lines
@@ -23,11 +24,11 @@ vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = "set fo-=c fo
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = highlight_group,
+    pattern = "*",
 })
 
 -- TODO: Convert these to lua
@@ -35,63 +36,62 @@ vim.cmd([[
 "" Move cursorline to active window
 autocmd VimEnter,WinEnter,BufWinEnter * setlocal cursorline
 autocmd WinLeave * setlocal nocursorline
-
 autocmd! TermOpen term://* lua set_terminal_keymaps()
 
 augroup bash
-  autocmd!
-  autocmd BufWritePre *.sh %s/\s\+$//e
+    autocmd!
+    autocmd BufWritePre *.sh %s/\s\+$//e
 augroup end
 
 augroup java
-  autocmd!
-  autocmd FileType java set colorcolumn=100
-  autocmd BufWritePre *.java %s/\s\+$//e
+    autocmd!
+    autocmd FileType java set colorcolumn=100
+    autocmd BufWritePre *.java %s/\s\+$//e
 augroup end
 
 augroup javascript
-  autocmd!
-  autocmd BufRead,BufNewFile  *.ts set filetype=typescript
-  autocmd BufRead,BufNewFile  *.jsx set filetype=javascript
-  autocmd FileType javascript set colorcolumn=100
-  autocmd BufWritePre *.js,*.ts %s/\s\+$//e
+    autocmd!
+    autocmd BufRead,BufNewFile  *.ts set filetype=typescript
+    autocmd BufRead,BufNewFile  *.jsx set filetype=javascript
+    autocmd FileType javascript set colorcolumn=100
+    autocmd BufWritePre *.js,*.ts %s/\s\+$//e
 augroup end
 
 augroup lua
-  autocmd!
-  autocmd BufWritePre *.lua %s/\s\+$//e
+    autocmd!
+    autocmd BufWritePre *.lua %s/\s\+$//e
 augroup end
 
 augroup markdown
-  autocmd!
-  autocmd BufWritePre *.md %s/\s\+$//e
-  autocmd FileType markdown set textwidth=100
-  autocmd FileType markdown set colorcolumn=100
+    autocmd!
+    autocmd BufWritePre *.md %s/\s\+$//e
+    autocmd FileType markdown set textwidth=100
+    autocmd FileType markdown set colorcolumn=100
 augroup end
 
 augroup python
-  autocmd!
-  autocmd FileType python set colorcolumn=100
-  autocmd BufRead,BufNewFile  *.ipynb set syntax=python
-  autocmd BufWritePre *.py %s/\s\+$//e
-  autocmd FileType python set foldmethod=indent
-  autocmd FileType python set foldexpr=''
+    autocmd!
+    autocmd FileType python set colorcolumn=100
+    autocmd BufRead,BufNewFile  *.ipynb set syntax=python
+    autocmd BufWritePre *.py %s/\s\+$//e
+    autocmd FileType python set foldmethod=indent
+    autocmd FileType python set foldexpr=''
 augroup end
 
 augroup scala
-  autocmd!
-  autocmd BufRead,BufNewFile *.sbt set filetype=scala
-  autocmd FileType scala set colorcolumn=100
-  autocmd BufWritePre *.scala,*.sbt %s/\s\+$//e
+    autocmd!
+    autocmd BufRead,BufNewFile *.sbt set filetype=scala
+    autocmd FileType scala set colorcolumn=100
+    autocmd BufWritePre *.scala,*.sbt %s/\s\+$//e
 augroup end
 
 augroup sql
-  autocmd!
-  autocmd BufWritePre *.sql %s/\s\+$//e
+    autocmd!
+    autocmd BufWritePre *.sql %s/\s\+$//e
 augroup end
 
 augroup vim
-  autocmd!
-  autocmd BufWritePre *.vim %s/\s\+$//e
+    autocmd!
+    autocmd BufWritePre *.vim %s/\s\+$//e
 augroup end
 ]])
