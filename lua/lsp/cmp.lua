35e792fd8a53b5d59a7fd750af58dc63f46ffddf
Jon <jonathan.p.lamar@gmail.com>
2023-01-19 08:43:29 -0700

    Disable preselect to stop auto completion when I don't want it

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -35,6 +35,8 @@ cmp.setup({
         end,
     },
 
+    preselect = cmp.PreselectMode.None,
+
     mapping = cmp.mapping.preset.insert({
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
