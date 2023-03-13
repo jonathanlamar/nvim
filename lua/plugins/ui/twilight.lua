9de85c222348688186269955bbca49ab40e7ab03
Jon <jonathan.p.lamar@gmail.com>
2023-03-12 21:33:48 -0600

    messing with zen mode

--- lua/plugins/ui/twilight.lua
+++ lua/plugins/ui/twilight.lua
@@ -3,10 +3,9 @@ return {
     lazy = true,
     opts = {
         dimming = {
-            -- TODO: This has no effect
             alpha = 0.25, -- amount of dimming
             -- we try to get the foreground from the highlight groups or fallback color
-            color = { "Normal", "#ffffff" },
+            --[[ color = { "Normal", "#ffffff" }, ]]
             inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
         },
         context = 10, -- amount of lines we will try to show around the current line
