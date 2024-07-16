0786b4dc530b59d96111ff81b74b209b9cb3a6ea
Jon Lamar <jonathan-lamar@pluralsight.com>
2024-07-16 10:01:13 -0600

    Update which key mappings to be compatible with new API

--- lua/plugins/ease/which-key.lua
+++ lua/plugins/ease/which-key.lua
@@ -28,18 +28,11 @@ return {
             separator = "➜", -- symbol used between a key and it's label
             group = "+", -- symbol prepended to a group
         },
-        window = {
-            border = "single", -- none, single, double, shadow
-            position = "bottom", -- bottom, top
-            margin = { 1, 0, 1, 0 }, -- eLxtra window margin [top, right, bottom, left]
-            padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-        },
         layout = {
             height = { min = 4, max = 25 }, -- min and max height of the columns
             width = { min = 20, max = 50 }, -- min and max width of the columns
             spacing = 3, -- spacing between columns
         },
-        hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
         show_help = true, -- show help message on the command line when the popup is visible
     },
 }
