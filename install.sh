f860dbe713d67fec1d5c62445c4757a0917a936f
Jon Lamar <jonathan.p.lamar@gmail.com>
2024-10-06 06:58:07 -0600

    Update install and uninstall to use pyenv

--- install.sh
+++ install.sh
@@ -1,36 +1,5 @@
 #!/usr/bin/env bash
 
-function usageFunction() {
-    echo -e "EXPLANATION: Installs the neovim config"
-    echo -e "USAGE:"
-    echo -e "install [--skip-cargo-pkgs] [--skip-golang-pkgs] [--skip-npm-pkgs] [--skip-python-pkgs]"
-    echo -e "    OPTIONS"
-    echo -e "        -n|--skip-npm-pkgs      : Whether to skip installing npm packages.  Optional, defaults to false."
-    echo -e "        -p|--skip-python-pkgs   : Whether to skip installing python packages.  Optional, defaults to false."
-    echo -e "        -h|--help               : prints this message."
-}
-
-SKIP_NPM_PKGS="false"
-SKIP_PYTHON_PKGS="false"
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -h|--help)
-            usageFunction
-            exit 0;;
-        -n|--skip-npm-pkgs)
-            SKIP_NPM_PKGS="true"
-            shift;;
-        -p|--skip-python-pkgs)
-            SKIP_PYTHON_PKGS="true"
-            shift;;
-        -*|--*)
-            echo "Unknown option $1"
-            exit 1;;
-        *)
-    esac
-done
-
-
 # Where is this script located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 NVIMDIR=$HOME/.config/nvim
@@ -52,17 +21,10 @@ echo "$NVIMDIR -> $SCRIPT_DIR"
 #==============
 # Install virtual environment
 #==============
-if [ "$SKIP_PYTHON_PKGS" == "false" ]; then
-    yes | conda create -n neovim python=3.10
-    conda run -n neovim bash -c "pip install -r requirements.txt"
-fi
-
-#==============
-# Install js/ts formatting and linting packages
-#==============
-if [ "$SKIP_NPM_PKGS" == "false" ]; then
-    npm install -g prettier eslint
-fi
+which pyenv || echo "PYENV NOT INSTALLED"; exit 1
+test -d $PYENV_ROOT/versions/3.10.14 || yes | pyenv install 3.10.14
+test -d $PYENV_ROOT/versions/3.10.14/envs/neovim || pyenv virtualenv 3.10.14 neovim
+pyenv activate neovim && pip install -r requirements.txt
 
 #==============
 # And we are done
--- uninstall.sh
+++ uninstall.sh
@@ -1,36 +1,7 @@
 #!/usr/bin/env bash
 
-function usageFunction() {
-    echo -e "EXPLANATION: Uninstalls the neovim config"
-    echo -e "USAGE:"
-    echo -e "uninstall [--skip-python-pkgs]"
-    echo -e "    OPTIONS"
-    echo -e "        -p|--skip-python-pkgs : Whether to skip uninstalling python packages.  Optional, defaults to false."
-    echo -e "        -h|--help             : prints this message."
-}
-
-SKIP_PYTHON_PKGS="false"
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -h|--help)
-            usageFunction
-            exit 0;;
-        -p|--skip-python-pkgs)
-            SKIP_PYTHON_PKGS="true"
-            shift;;
-        -*|--*)
-            echo "Unknown option $1"
-            exit 1;;
-        *)
-    esac
-done
-
 rm -rf ~/.local/share/nvim
 rm -rf ~/.local/state/nvim
 rm -rf ~/.config/nvim
 rm -rf ~/.cache/nvim
 rm -rf ./lazy-lock.json
-
-if [ "$SKIP_PYTHON_PKGS" == "false" ]; then
-    conda env remove -n neovim
-fi
