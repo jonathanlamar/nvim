626f6df9d337f54c6441b1754a20d099e78d71f3
Jon <jonathan.p.lamar@gmail.com>
2025-12-14 16:33:54 -0700

    Get rid of terminal autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -25,16 +25,6 @@ vim.api.nvim_create_autocmd("WinLeave", {
     end,
 })
 
-local set_terminal_keymaps = function()
-    local opts = { noremap = true }
-    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
-end
-vim.api.nvim_create_autocmd("TermOpen", { pattern = "term://*", callback = set_terminal_keymaps })
-
 vim.api.nvim_create_augroup("text", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
     group = "text",
