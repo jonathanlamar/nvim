63c5d4faaacfcefb21ec42c3bf2fe6154a7efbcc
Jon <jonathan.p.lamar@gmail.com>
2025-12-20 20:33:10 -0600

    Split out lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -6,49 +6,4 @@ vim.lsp.config("*", {
     capabilities = capabilities,
 })
 
-vim.lsp.config["pyright"] = {
-    cmd = { "pyright-langserver", "--stdio" },
-    filetypes = { "python" },
-    root_markers = {
-        "pyrightconfig.json",
-        "pyproject.toml",
-        "setup.py",
-        "setup.cfg",
-        "requirements.txt",
-        "Pipfile",
-        ".git",
-    },
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                diagnosticMode = "openFilesOnly",
-            },
-        },
-    },
-}
-
-vim.lsp.config["luals"] = {
-    cmd = { "lua-language-server" },
-    filetypes = { "lua" },
-    root_markers = {
-        ".emmyrc.json",
-        ".luarc.json",
-        ".luarc.jsonc",
-        ".luacheckrc",
-        ".stylua.toml",
-        "stylua.toml",
-        "selene.toml",
-        "selene.yml",
-        ".git",
-    },
-    settings = {
-        Lua = {
-            codeLens = { enable = true },
-            hint = { enable = true, semicolon = "Disable" },
-        },
-    },
-}
-
 vim.lsp.enable({ "pyright", "luals" })
